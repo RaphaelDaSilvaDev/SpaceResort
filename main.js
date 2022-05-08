4a4f473480cd517cb0b30355207910d78a01ce66
RaphaelDaSilvaDev <raphael.h.silva@protonmail.com>
2022-05-07 22:18:29 -0300

    Add slider, Add ScrollReveal, customization styles

--- main.js
+++ main.js
@@ -40,3 +40,36 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  keyboard: true,
+  simulateTouch: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  breakpoints: {
+    1023: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+      spaceBetween: 40
+    }
+  }
+})
+
+ScrollReveal({ origin: 'top', distance: '30px', duration: 700 }).reveal(`
+#home,
+#home img,
+#services,
+#services .card,
+#depositions,
+#depositions .swiper-wrapper,
+#about,
+#about .stats,
+#contact,
+footer
+`)
